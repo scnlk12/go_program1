afb1fac0172429370b81e3ebc18cdd12d32f2244
fan <1206203790@qq.com>
2025-11-13 20:19:44 +0800

    refactor: 用户业务层封装

--- src/server.go
+++ src/server.go
@@ -9,7 +9,7 @@ import (
 
 // 创建一个Server类
 type Server struct {
-	Ip string
+	Ip   string
 	Port int
 
 	// 增加在线用户map
@@ -21,20 +21,19 @@ type Server struct {
 	Message chan string
 }
 
-
 // 创建一个server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip: ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
 		OnlineMap: make(map[string]*User),
-		Message: make(chan string),
+		Message:   make(chan string),
 	}
 	return server
 }
 
 // 监听Message广播消息channel的goroutine，一旦有消息就会发送给所有的在线user
-func (server *Server)ListenMessager()  {
+func (server *Server) ListenMessager() {
 	for {
 		// 监听channel
 		msg := <-server.Message
@@ -61,14 +60,9 @@ func (server *Server) Handler(conn net.Conn) {
 	// fmt.Println("链接建立成功")
 
 	// 用户上线 将用户加入到onlineMap中
-	user := NewUser(conn)
+	user := NewUser(conn, server)
 
-	server.mapLock.Lock()
-	server.OnlineMap[user.Name] = user
-	server.mapLock.Unlock()
-
-	// 广播当前用户上线消息
-	server.BroadCast(user, "is online.\n")
+	user.Online()
 
 	// 接收用户输入信息，将消息进行广播
 	go func() {
@@ -77,10 +71,10 @@ func (server *Server) Handler(conn net.Conn) {
 			n, err := conn.Read(buf)
 			// 关闭客户端请求时触发
 			if n == 0 {
-				server.BroadCast(user, "is not online...")
-				return 
+				user.Offline()
+				return
 			}
-			
+
 			if err != nil && err != io.EOF {
 				fmt.Println("Conn Read Err:", err)
 				return
@@ -88,13 +82,14 @@ func (server *Server) Handler(conn net.Conn) {
 
 			// 提取用户消息 去除\n
 			msg := string(buf[:n-1])
-			// 将得到的消息进行广播
-			server.BroadCast(user, msg)
+			
+			// 用户针对msg进行消息处理
+			user.DoMessage(msg)
 		}
 	}()
 
 	// 当前handler阻塞
-	
+
 }
 
 // 启动服务器的接口
--- src/user.go
+++ src/user.go
@@ -4,21 +4,23 @@ import "net"
 
 // 创建User类
 type User struct {
-	Name string
-	Addr string
-	C    chan string
-	conn net.Conn
+	Name   string
+	Addr   string
+	C      chan string
+	conn   net.Conn
+	server *Server
 }
 
 // 新建User方法
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C: make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
 	}
 
 	// 启动监听当前user channel消息的goroutine
@@ -30,8 +32,35 @@ func NewUser(conn net.Conn) *User {
 // 监听当前User channel的方法，一旦有消息就直接发送给对端客户端
 func (user *User) ListenMessage() {
 	for {
-		msg := <- user.C
+		msg := <-user.C
 
 		user.conn.Write([]byte(msg + "\n"))
 	}
+}
+
+// 用户的上线功能
+func (user *User) Online() {
+	// 用户上线 将用户加入到onlineMap中
+	user.server.mapLock.Lock()
+	user.server.OnlineMap[user.Name] = user
+	user.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	user.server.BroadCast(user, "is online.\n")
+}
+
+// 用户的下线功能
+func (user *User) Offline() {
+	// 用户下线 将用户从onlineMap中删除
+	user.server.mapLock.Lock()
+	delete(user.server.OnlineMap, user.Name)
+	user.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	user.server.BroadCast(user, "is offline.\n")
+}
+
+// 用户处理消息
+func (user *User) DoMessage(msg string) {
+	user.server.BroadCast(user, msg)
 }
