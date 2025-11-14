6f73cb8b8aa248757eb5fcf4224652f95c909521
fan <1206203790@qq.com>
2025-11-14 11:50:22 +0800

    feat: 菜单显示

--- src/client.go
+++ src/client.go
@@ -8,17 +8,19 @@ import (
 
 // 定义Client类
 type Client struct {
-	ServerIp string
+	ServerIp   string
 	ServerPort int
-	Name string
-	conn net.Conn
+	Name       string
+	conn       net.Conn
+	input      int
 }
 
 // 创建Client对象
 func NewClient(serverIp string, serverPort int) *Client {
-	client := &Client {
-		ServerIp: serverIp,
+	client := &Client{
+		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		input:      999,
 	}
 
 	// 连接server
@@ -31,6 +33,48 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client.conn = conn
 
 	return client
+}
+
+// 显示菜单
+func (client *Client) menu() bool {
+	var input int
+
+	fmt.Println("1. BroadCast...")
+	fmt.Println("2. Private Chat...")
+	fmt.Println("3. Change Name...")
+	fmt.Println("0. Exit")
+
+	// 接收用户输入
+	fmt.Scanln(&input)
+
+	if input >= 0 && input <= 3 {
+		client.input = input
+		return true
+	} else {
+		fmt.Println("请输入合法范围内的数字")
+		return false
+	}
+
+}
+
+// Run
+func (client *Client) Run() {
+	for client.input != 0 {
+		for client.menu() != true {
+		}
+
+		// 根据不同模式处理不同操作
+		switch client.input {
+		case 1:
+			fmt.Println("群聊...")
+			break
+		case 2:
+			fmt.Println("私聊...")
+			break
+		case 3:
+			fmt.Println("更改用户名...")
+		}
+	}
 }
 
 // 执行顺序 pkg -> const -> var -> init -> main
@@ -58,4 +102,5 @@ func main() {
 	}
 
 	fmt.Println("连接服务器成功")
+	client.Run()
 }
